e6ad4e5c0dc5a10ae8d47ac04a45e1d908dbc379
franmonty77 <franmontoya777@gmail.com>
2025-09-13 12:55:52 +0200

    prueba

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::get('/sesiones', [SesionController::class, 'index'])->name('sesiones.inde
 Route::post('/sesiones/filter', [SesionController::class, 'filter'])->name('sesiones.filter');
 Route::get('/sesiones/clear', [SesionController::class, 'clear'])->name('sesiones.clear');
 
-// Rutas para las reservas (web)
+// Rutas para las reservas (webs)
 Route::middleware('api.auth')->group(function() {
     Route::get('/reservas', [App\Http\Controllers\ReservaController::class, 'index'])->name('reservas.index');
     Route::get('/reservas/crear', [App\Http\Controllers\ReservaController::class, 'create'])->name('reservas.create');
